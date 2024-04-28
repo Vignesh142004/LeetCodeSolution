97f32164c2f5e1bc8915b1a6ad5e3d09611df357
Vignesh142004 <139527479+Vignesh142004@users.noreply.github.com>
2024-04-28 10:52:15 +0530

    Time: 40 ms (8.85%), Space: 80 MB (20.2%) - LeetHub

---
+++ 3373-maximum-prime-difference/3373-maximum-prime-difference.java
@@ -0,0 +1,40 @@
+class Solution {
+      boolean isprime(int n)
+       {
+            if(n<=1)
+            {
+                return false;
+            }
+            for(int j=2;j<=Math.sqrt(n);j++)
+            {
+                if(n%j==0)
+                {
+                   return false;
+                }
+            }
+            return true;
+    }
+    public int maximumPrimeDifference(int[] nums) {
+        List<Integer> arr=new ArrayList<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(isprime(nums[i]))
+            {
+                arr.add(i);
+            }
+        }
+        int ans=0;
+        Collections.sort(arr);
+        System.out.println(arr);
+        if(arr.size()==1)
+        {
+            return 0;
+        }
+        else
+        {
+            ans=arr.get(arr.size()-1)-arr.get(0);
+        }
+        return ans;
+        
+    }}
