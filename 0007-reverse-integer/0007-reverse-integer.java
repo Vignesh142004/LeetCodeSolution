41c7378c560682d22325615f1005d2c6faa02812
Vignesh142004 <139527479+Vignesh142004@users.noreply.github.com>
2024-04-28 08:35:36 +0530

    Time: 1 ms (83.32%), Space: 41 MB (31.75%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int reverse(int x) {
+         int rev=0;
+         boolean neg=false;
+        
+        if(x>Integer.MAX_VALUE||x<Integer.MIN_VALUE)
+        {
+            return 0;
+        }
+        if (x < 0) {
+            x = -x;
+            neg = true;
+        }
+        
+       
+        while(x!=0){
+             int rem=x%10;
+             if (rev > (Integer.MAX_VALUE - rem) / 10) return 0;
+            rev=(rev*10)+rem;
+            x=x/10;  
+        }
+         return neg?-rev:rev;  
+    }
+}
