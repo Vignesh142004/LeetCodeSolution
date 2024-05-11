232631a62334456efb245aafa3fed3a91f533ff7
Vignesh142004 <139527479+Vignesh142004@users.noreply.github.com>
2024-05-11 07:28:49 +0530

    Time: 1 ms (99.77%), Space: 53 MB (34.76%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int repeat=nums[0];
+        int count=1;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]==repeat)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+            if(count==0)
+            {
+                repeat=nums[i];
+                count=1;
+            }
+        }
+        return repeat;
+        
+        
+    }
+}
