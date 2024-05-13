4a49dd2336fd14a9854c4da69f96438b93db2096
Vignesh142004 <139527479+Vignesh142004@users.noreply.github.com>
2024-05-13 07:30:07 +0530

    Time: 1 ms (38.07%), Space: 43.9 MB (93.82%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    void func(TreeNode root,List<Integer> arr)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        func(root.left,arr);
+        arr.add(root.val);
+        func(root.right,arr);
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> arr=new ArrayList<>();
+        func(root,arr);
+        return arr.get(k-1);
+        
+    }
+}
