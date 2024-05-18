5ecb52d48afa2c0d7f4b1445ae526d4dc92680be
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2024-05-18 18:18:50 +0530

    Time: 0 ms (100%), Space: 41.5 MB (45.82%) - LeetHub

---
+++ 0909-stone-game/0909-stone-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+        /*int first=piles[0];
+        int last=piles[piles.length-1];
+        if(first==last)
+        {
+            return true;
+        }
+        return false;*/
+        
+    }
+
+}
