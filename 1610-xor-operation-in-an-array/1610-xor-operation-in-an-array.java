3a6a77880ff03378287c76f23daa46fe50c9792e
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2024-05-18 18:01:30 +0530

    Time: 0 ms (100%), Space: 40 MB (84.95%) - LeetHub

---
+++ 1610-xor-operation-in-an-array/1610-xor-operation-in-an-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=start+(2*i);
+        }
+        int ans=a[0];
+        for(int i=1;i<n;i++)
+        {
+            ans^=a[i];
+        }
+         return ans;
+
+
+        
+    }
+   
+}
