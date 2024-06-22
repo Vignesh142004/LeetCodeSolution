973e042d3296f65dbff7a93b11a2d147b346f33e
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2024-06-22 10:33:05 +0530

    Time: 4 ms (82.59%), Space: 43.4 MB (64.94%) - LeetHub

---
+++ 0987-reveal-cards-in-increasing-order/0987-reveal-cards-in-increasing-order.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int n=deck.length;
+        int ans[]=new int[n];
+        Arrays.sort(deck);
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<n;i++)
+        {
+            q.add(i);
+        }
+        for(int i=0;i<n;i++)
+        {
+            ans[q.peek()]=deck[i];
+            q.remove();
+            q.add(q.poll());
+        }
+        return ans;
+    
+
+        
+    }
+}
