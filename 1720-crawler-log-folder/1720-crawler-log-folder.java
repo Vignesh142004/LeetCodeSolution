80777d72e782413a0604529ebb769ac91112a344
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2024-06-22 16:44:08 +0530

    Time: 1 ms (99.3%), Space: 42.3 MB (15.04%) - LeetHub

---
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int count=0;
+        for(String s:logs)
+        {
+            if(s.equals("../"))
+            {
+                if(count==0)
+                {
+                    continue;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+            else if(s.equals("./"))
+            {
+                continue;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
