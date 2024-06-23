93edc9d4e6c0607b72a189ed84cf8e70a8c4c288
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2024-06-23 10:15:56 +0530

    Time: 1 ms (20.42%), Space: 41.8 MB (22.59%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,7 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
         int n=nums.length;
-        for(int i=0;i<n;i++)
+       /* for(int i=0;i<n;i++)
         {
             for(int j=0;j<n-i-1;j++)
             {
@@ -12,11 +12,37 @@ class Solution {
                     nums[j+1]=temp;
                 }
             }
+        }*///bubble sort
+        /*for(int i=1;i<n;i++)
+        {
+            int key=nums[i];
+            int j=i-1;
+            while(j>=0 && nums[j]>key)
+            {
+                nums[j+1]=nums[j];
+                j--;
+            }
+            nums[j+1]=key;
+        }*/ //insertion sort
+        for(int i=0;i<n;i++)
+        {
+            int minidx=i;
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[minidx]>nums[j])
+                {
+                    minidx=j;
+                }
+            }
+            int temp=nums[minidx];
+            nums[minidx]=nums[i];
+            nums[i]=temp;
         }
-        //Arrays.sort(nums);
-
-        System.out.println(nums);
         
+        for(int i=0;i<n;i++)
+        {
+
+        }
         
     }
 }
