2448313eff019cca17bc009ec94d20aa1191284c
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2024-06-26 14:40:46 +0530

    Time: 0 ms (100%), Space: 40.2 MB (69.37%) - LeetHub

--- 2383-add-two-integers/2383-add-two-integers.java
+++ 2383-add-two-integers/2383-add-two-integers.java
@@ -1,6 +1,16 @@
 class Solution {
     public int sum(int num1, int num2) {
-         return num1+num2;
+         int ans=0;
+         int carry=0;
+         while(num2!=0)
+         {
+            ans=num1^num2;
+            carry=num1&num2;
+            carry=carry<<1;
+            num1=ans;
+            num2=carry;
+         }
+         return num1;
         
     }
 }
