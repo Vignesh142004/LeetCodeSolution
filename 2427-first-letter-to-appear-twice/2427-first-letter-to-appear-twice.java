926bd6d2be1979604b6285b084fb6e36d2e140b8
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2024-07-05 11:02:02 +0530

    Time: 119 ms (21.13%), Space: 41.3 MB (67.78%) - LeetHub

---
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
@@ -0,0 +1,19 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        int n=s.length();
+        HashMap<Character,Integer> hm=new HashMap<>();
+        for(int i=0;i<n;i++)
+        {
+            if(hm.containsKey(s.charAt(i)))
+            {
+                return s.charAt(i);
+            }
+            else
+            {
+                hm.put(s.charAt(i),1);
+            }
+        }
+        return s.charAt(0);
+        
+    }
+}
