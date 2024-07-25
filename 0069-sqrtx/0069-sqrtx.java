043bb84423e58ecaf853f8b607b355a4b62ff2ae
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2024-07-25 20:49:31 +0530

    Time: 0 ms (100%), Space: 41.3 MB (5.46%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,26 +1,28 @@
 
 class Solution {
     public int mySqrt(int x) {
-        int first=1;
-        int last=x;
-        while(first<=last)
-        {
-            int mid=(first+last)/2;
-            if(x/mid==mid)
-            {
-                return mid;
-            }
-            else if(x/mid<mid)
-            {
-                last=mid-1;
-            }
-            else
-            {
-                first=mid+1;
-            }
+        // int first=1;
+        // int last=x;
+        // while(first<=last)
+        // {
+        //     int mid=(first+last)/2;
+        //     if(x/mid==mid)
+        //     {
+        //         return mid;
+        //     }
+        //     else if(x/mid<mid)
+        //     {
+        //         last=mid-1;
+        //     }
+        //     else
+        //     {
+        //         first=mid+1;
+        //     }
             
-        }
-        return last;
+        // }
+        // return last;
+        int ans=(int)Math.sqrt(x);
+        return ans;
         
         
     }
