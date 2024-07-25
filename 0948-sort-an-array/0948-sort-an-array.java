25617cdc4951b7fe0086257ea210d492cc744d9d
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2024-07-25 07:47:14 +0530

    Time: 17 ms (89.75%), Space: 55.5 MB (71.97%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,22 +1,21 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        int n=nums.length;
-        /*for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n-i-1;j++)
-            {
-                if(nums[j]>nums[j+1])
-                {
-                    int temp=nums[j];
-                    nums[j]=nums[j+1];
-                    nums[j+1]=temp;
-
-                }
-            }
-        }
-        return nums;*/ 
+        // int n=nums.length;
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<n-i-1;j++)
+        //     {
+        //         if(nums[j]>nums[j+1])
+        //         {
+        //             int temp=nums[j];
+        //             nums[j]=nums[j+1];
+        //             nums[j+1]=temp;
+        //         }
+        //     }
+        // }
         Arrays.sort(nums);
-        return nums;
+         return nums;
+        
         
     }
 }
