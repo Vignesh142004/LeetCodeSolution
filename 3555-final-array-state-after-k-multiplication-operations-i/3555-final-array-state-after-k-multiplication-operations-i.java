dd3d9c4d430b002dcca05ac04cbd092f794739fa
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2024-08-28 08:03:23 +0530

    Time: 1 ms (100%), Space: 44.9 MB (31.2%) - LeetHub

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] getFinalState(int[] arr, int k, int mul) {
+        int n=arr.length;
+        while(k>0)
+        {
+            int min=Integer.MAX_VALUE;
+            int idx=-1;
+            for(int i=0;i<n;i++)
+            {
+                if(arr[i]<min)
+                {
+                    min=arr[i];
+                    idx=i;
+                }
+            }
+            if(idx==-1)
+            {
+                continue;
+            }
+            else
+            {
+                arr[idx]=mul*arr[idx];
+            }
+            k--;
+        }
+        return arr;
+        
+    }
+}
