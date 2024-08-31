dd138cc06d3178366192b201d1d2157ded2dc64d
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2024-08-31 07:30:34 +0530

    Time: 1 ms (34.01%), Space: 41.8 MB (20.51%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,16 +1,12 @@
 public class Solution {
     public int lengthOfLastWord(String s) {
-        
         if (s == null || s.length() == 0) {
             return 0;
         }
-        
         String[] strs = s.split(" ");
-        
         if (strs.length == 0) {
             return 0;
         }
-        
         return strs[strs.length - 1].length();
     }
 }
