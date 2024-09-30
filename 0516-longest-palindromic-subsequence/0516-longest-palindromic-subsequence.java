6f8b1c4bb0dafe9f313a012a1fb558b93803b8e1
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2024-09-30 21:39:55 +0530

    Time: 65 ms (14.04%), Space: 46.7 MB (87.24%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,47 +1,48 @@
 class Solution {
-    public int longestPalindromeSubseq(String s) 
+    static int lps(String s1,String s2)
     {
-        
-     String s1="";
-        for(int i=s.length()-1;i>=0;i--)
+        int l1=s1.length();
+        int l2=s2.length();
+        int dp[][]=new int[l1+1][l2+1];
+        for(int i=0;i<=l1;i++)
         {
-            s1=s1+s.charAt(i);
-        }
-        int m=s.length();
-        int n=s1.length();
-        int dp[][]=new int[m+1][n+1];
-        for(int i=0;i<=m;i++)
-        {
-            for(int j=0;j<=n;j++)
+            for(int j=0;j<=l2;j++)
             {
                 dp[i][j]=-1;
             }
         }
-        int ans=lcs(s,s1,m,n,dp);
-        return ans;
-    }
-    
-        int lcs(String s,String s1,int m,int n,int dp[][])
+        for(int i=0;i<=l1;i++)
         {
-            if(m==0||n==0)
+            dp[i][0]=0;
+        }
+        for(int i=0;i<=l2;i++)
+        {
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=l1;i++)
+        {
+            for(int j=1;j<=l2;j++)
             {
-                return 0;
-            }
-            if(dp[m][n]!=-1)
-            {
-                return dp[m][n];
-            }
-            if(s.charAt(m-1)==s1.charAt(n-1))
-            {
-                dp[m][n]=1+lcs(s,s1,m-1,n-1,dp);
-                return dp[m][n];
-            }
-            else
-            {
-                int ans1=lcs(s,s1,m-1,n,dp);
-                int ans2=lcs(s,s1,m,n-1,dp);
-                dp[m][n]= Math.max(ans1,ans2);
-                return dp[m][n];
+                if(s1.charAt(i-1)==s2.charAt(j-1))
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
+        return dp[l1][l2];
+    }
+    public int longestPalindromeSubseq(String s) {
+        String rev="";
+        int n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            rev=s.charAt(i)+rev;
+        }
+        int ans=lps(s,rev);
+        return ans;
+    }
 }
