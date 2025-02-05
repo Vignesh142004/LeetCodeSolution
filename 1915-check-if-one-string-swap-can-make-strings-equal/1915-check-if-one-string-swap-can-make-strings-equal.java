4b07c20256cbcc5f1642152cd385f98af124f55a
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2025-02-05 09:42:53 +0530

    Time: 4 ms (8.32%), Space: 41.9 MB (15.64%) - LeetHub

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        int n=s1.length();
+        int mismatch=0;
+        int cnt=0;
+        HashMap<Character,Integer> hm=new HashMap<>();
+        for(char ch:s1.toCharArray())
+        {
+            if(hm.containsKey(ch))
+            {
+                hm.put(ch,hm.get(ch)+1);
+            }
+            else
+            {
+                hm.put(ch,1);
+            }
+        }
+        for(char ch:s2.toCharArray())
+        {
+            if(hm.containsKey(ch))
+            {
+                hm.put(ch,hm.get(ch)-1);
+            }
+            else
+            {
+                hm.put(ch,1);
+            }
+        }
+        for(Map.Entry<Character,Integer> m:hm.entrySet())
+        {
+            if(m.getValue()!=0)
+            {
+                cnt++;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+           if(s1.charAt(i)!=s2.charAt(i))
+           {
+             mismatch++;
+           }
+        }
+        if((mismatch==0 || mismatch==2) && cnt==0)
+        {
+            return true;
+        }
+        return false;
+        
+    }
+}
