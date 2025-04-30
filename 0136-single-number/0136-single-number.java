c28eb28b4f150e92c7543ca4c24d0c1d3a793cb7
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2025-04-30 09:33:22 +0530

    Time: 1 ms (99.92%), Space: 46.1 MB (71.55%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int n=nums.length;
-        for(int i=0;i<n;i++)
+        /*for(int i=0;i<n;i++)
         {
             int count=0;
             for(int j=0;j<n;j++)
@@ -17,9 +17,15 @@ class Solution {
             return nums[i];
             }
             
+        }*/
+        int xor=0;
+        for(int i=0;i<n;i++)
+        {
+            xor=xor^nums[i];
         }
+        return xor;
         
 
-       return 0; 
+       
     }
 }
