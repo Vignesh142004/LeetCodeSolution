ad4227d2381b9d78a5838f03274c3e38ef4c1e83
VIGNESH R <139527479+Vignesh142004@users.noreply.github.com>
2026-02-14 09:49:25 +0530

    Time: 278 ms (68.89%), Space: 0B (100%) - LeetHub

---
+++ 3793-find-users-with-high-token-usage/3793-find-users-with-high-token-usage.sql
@@ -0,0 +1,1 @@
+Select user_id,count(prompt) as prompt_count,round(avg(tokens),2) as avg_tokens from prompts group by user_id  having prompt_count>=3 and max(tokens)>avg_tokens order by avg_tokens desc;
